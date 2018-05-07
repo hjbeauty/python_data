5e2a3626e616a27e20f3faf65f2cd32c4a15268a
Myang <kmh4503@gmail.com>
2018-05-08 00:46:05 +0900

    시간대별 사용량 차트로 그리기_PC version만

--- talk.py
+++ talk.py
@@ -1,21 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# =====Simple analysis of kakaotalk chat =====
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# =====Simple analysis of kakaotalk chat =====
-
 # Mobile(IOS)로 내보낸 대화내용을 dataframe에 넣기
-# PC version으로 내보낸 대화내용을 dataframe에 넣기
-# Android 로 내보낸 대화내용을 dataframe에 넣기 
-# 차트 표현하기(plot) _ 시간대별/일별 은 매개변수로 넣기 
-
-# 시간대별 사용량 
-def show_daily_usage(file_name, file_ver):
- #hours = []
+def ios_parse_to_df(file_name):
+    hours = []
+    for i in range(24):
+         hours.append(i)
     hour = ''
-    # for i in range(24):
-    #     hours.append(i)
-    df = pd.DataFrame()
+    df = pd.DataFrame(columns=hours)
+    print('ios')
+    return df
+
+# PC version으로 내보낸 대화내용을 dataframe에 넣기
+def pc_parse_to_df(file_name):
+    hours = []
+    for i in range(24):
+         hours.append(i)
+    hour = ''
+    df = pd.DataFrame(columns=hours)
     with open(file_name, 'r', encoding='utf-8') as file: 
         line = file.readline()
         count = 1
@@ -25,10 +31,10 @@ def show_daily_usage(file_name, file_ver):
                 break
             if(line != '\n' and line[0] == '-'):
                 date = line.strip('-') #뒷부분 strip이 잘 안됨 (issue#1)
-                year = date.split(' ')[1]
-                month = date.split(' ')[2]
-                day = date.split(' ')[3]
-                date = year+month+day
+                year = date.split(' ')[1][:-1]
+                month = date.split(' ')[2][:-1]
+                day = date.split(' ')[3][:-1]
+                date = year+'-'+month+'-'+day
             if(line != '\n' and line[0] == '['):
                 linelist = line.split(' ')
                 prev_hour = hour
@@ -39,17 +45,49 @@ def show_daily_usage(file_name, file_ver):
                 else:
                     df.loc[date, hour] = count
                     count = 1
-        print(df)
+        df = df.fillna(0)
+        return df
         # df.to_csv('talktalk', sep='\t', encoding='utf-8')
+    
+# Mobile(Android) 로 내보낸 대화내용을 dataframe에 넣기 
+def android_parse_to_df(file_name):
+    print('android')
+    hours = []
+    for i in range(24):
+         hours.append(i)
+    df = pd.DataFrame(columns=hours)
+    return df
+
+# 차트 표현하기(plot) _ 시간대별/일별 은 매개변수로 넣기
+def visualize(df, file_name):
+    fig = plt.figure()
+    df.mean().plot(kind='bar')
+    new_file_name = file_name[:-4] + '.png'
+    fig.savefig(new_file_name)
+    # df.to_excel('test.xlsx', sheet_name='Sheet1' )
+    print(new_file_name + '으로 저장되었습니다.\n')
+
+# 1. 시간대별 사용량
 def show_time_usage(file_name, file_ver):
+    if(file_ver.upper() == 'PC'):
+        df = pc_parse_to_df(file_name)
+        print(df.tail())
+    elif(file_ver.upper() == 'IOS'):
+        df = ios_parse_to_df(file_name)
+    else:
+        df = android_parse_to_df(file_name)
+    visualize(df, file_name)
+
+
+# 2. 일별 사용량
+def show_day_usage(file_name, file_ver):
     print('show time_usage')
 
-
-# 특정 단어를 입력받아, 전체 대화내용 중에서 단어의 개수(사용자별) 알려주기
+# 3. 특정 단어를 입력받아, 전체 대화내용 중에서 단어의 개수(사용자별) 알려주기
 # 아직 형태소분석이 안되서 한글자 밖에 못함 ㅜㅜ 
 # total count까지만 함 / 사용자별 count dictionary에 저장해야함
 def count_word(file_name):
-    find_word = input('찾는 단어가 무엇인가요? \n')
+    find_word = input('찾고싶은 단어가 무엇인가요? \n')
     count = 0
     with open(file_name, 'r', encoding='utf-8') as file: 
         for line in file:
@@ -58,13 +96,11 @@ def count_word(file_name):
                     count+=1
         print("count:",count)
 
-
-
 # 사용자 입력 받기
 def get_user_input(file_name='',file_ver='', init=0):
     if(init == 0):
         print("========================")
-        print('kakaotalk 분석기 :-) \n')
+        print(' ▶▶  kakaotalk 분석기 :-) ◀◀ \n')
         file_name = input('내보내기 한 대화 파일이름을 입력해주세요.(파일이름.txt)\n')
         file_ver = input('어디에서 내보내기 했나요? (PC/IOS/Android)\n')
     print('-----------MENU-------------')
@@ -77,14 +113,14 @@ def get_user_input(file_name='',file_ver='', init=0):
     print(' ')
     return file_name, file_ver, user_input
 
-
+# Main
 if __name__ == '__main__':
     file_name, file_ver, user_input = get_user_input()
     while(True):
         if(user_input == '1'):
-            show_daily_usage(file_name, file_ver)
-        elif (user_input == '2'):
             show_time_usage(file_name, file_ver)
+        elif (user_input == '2'):
+            show_day_usage(file_name, file_ver)
         elif (user_input == '3'):
             count_word(file_name)
         else:
@@ -93,6 +129,3 @@ if __name__ == '__main__':
                 print('============종료 ============')
                 break
         filename, file_ver, user_input = get_user_input(file_name, file_ver, 1)
-
-
-   
