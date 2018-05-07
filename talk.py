2ec3124a05ae5f3e51b8d4fcf15ce593551ba37b
Myang <kmh4503@gmail.com>
2018-05-07 15:33:13 +0900

    create UI, define functions needed

--- talk.py
+++ talk.py
@@ -1,25 +1,22 @@
 import pandas as pd
 import numpy as np
-#import datetime
+import matplotlib.pyplot as plt
 
-# ===Simple analysis of kakaotalk chat ===
+# =====Simple analysis of kakaotalk chat =====
 
-### ideas ### 
-#user interface: open할 파일명, oo와의 대화, 분석결과 etc.
-#use 'find' method to get # of certain word(or emoji)
-#단체카톡: differently .
-#dataframe에 저장
-#차트 그려주기
+# Mobile(IOS)로 내보낸 대화내용을 dataframe에 넣기
+# PC version으로 내보낸 대화내용을 dataframe에 넣기
+# Android 로 내보낸 대화내용을 dataframe에 넣기 
+# 차트 표현하기(plot) _ 시간대별/일별 은 매개변수로 넣기 
 
-# !! NEED TO MAKE IN MODULE 
-
-if __name__ == '__main__':
-    #hours = []
+# 시간대별 사용량 
+def show_daily_usage(file_name, file_ver):
+ #hours = []
     hour = ''
     # for i in range(24):
     #     hours.append(i)
     df = pd.DataFrame()
-    with open('talk_short.txt', 'r', encoding='utf-8') as file: 
+    with open(file_name, 'r', encoding='utf-8') as file: 
         line = file.readline()
         count = 1
         while line != '':
@@ -43,4 +40,59 @@ if __name__ == '__main__':
                     df.loc[date, hour] = count
                     count = 1
         print(df)
-        df.to_csv('talktalk', sep='\t', encoding='utf-8')
+        # df.to_csv('talktalk', sep='\t', encoding='utf-8')
+def show_time_usage(file_name, file_ver):
+    print('show time_usage')
+
+
+# 특정 단어를 입력받아, 전체 대화내용 중에서 단어의 개수(사용자별) 알려주기
+# 아직 형태소분석이 안되서 한글자 밖에 못함 ㅜㅜ 
+# total count까지만 함 / 사용자별 count dictionary에 저장해야함
+def count_word(file_name):
+    find_word = input('찾는 단어가 무엇인가요? \n')
+    count = 0
+    with open(file_name, 'r', encoding='utf-8') as file: 
+        for line in file:
+            for word in line:
+                if(word == find_word):
+                    count+=1
+        print("count:",count)
+
+
+
+# 사용자 입력 받기
+def get_user_input(file_name='',file_ver='', init=0):
+    if(init == 0):
+        print("========================")
+        print('kakaotalk 분석기 :-) \n')
+        file_name = input('내보내기 한 대화 파일이름을 입력해주세요.(파일이름.txt)\n')
+        file_ver = input('어디에서 내보내기 했나요? (PC/IOS/Android)\n')
+    print('-----------MENU-------------')
+    print('1. 시간대별 카톡 사용량을 보고싶어요')
+    print('2. 일별 카톡사용량을 보고싶어요')
+    print('3. 누가 얼마나 OO 단어/이모티콘을 쓰는지 알고싶어요')
+    print('4. 종료')
+    print('-----------------------------')
+    user_input = input('숫자를 입력하세요 :')
+    print(' ')
+    return file_name, file_ver, user_input
+
+
+if __name__ == '__main__':
+    file_name, file_ver, user_input = get_user_input()
+    while(True):
+        if(user_input == '1'):
+            show_daily_usage(file_name, file_ver)
+        elif (user_input == '2'):
+            show_time_usage(file_name, file_ver)
+        elif (user_input == '3'):
+            count_word(file_name)
+        else:
+            answer = input('정말 그만할래요? (y/n) : ')
+            if(answer == 'y'):
+                print('============종료 ============')
+                break
+        filename, file_ver, user_input = get_user_input(file_name, file_ver, 1)
+
+
+   
